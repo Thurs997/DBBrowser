6625526c487a2c9b7826148a151172c3914562c0
Bartek Andrzejczak <ba.andrzejczak@gmail.com>
2014-05-31 12:46:11 +0200

    Struktura projektu

---
+++ RequestProcessor/src/main/java/pl/edu/pw/ii/DBBrowser/RequestProcessor/HttpRequest.java
@@ -0,0 +1,7 @@
+package pl.edu.pw.ii.DBBrowser.RequestProcessor;
+
+/**
+ * Created by kokoss on 5/31/14.
+ */
+public class HttpRequest {
+}
---
+++ RequestProcessor/src/test/java/pl/edu/pw/ii/DBBrowser/RequestProcessor/HttpRequestTest.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.ii.DBBrowser.RequestProcessor;
+import org.junit.Test;
+/**
+ * Created by kokoss on 5/31/14.
+ */
+public class HttpRequestTest {
+    @Test
+    public void firstTest(){
+
+    }
+
+
+}
